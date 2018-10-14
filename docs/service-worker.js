6652ba0d79b183603aa8ca2378cd64113e925592
ljj <ljjgit@qq.com>
2018-10-14 20:36:59 +0800

    存档

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/ja/precache-manifest.8bf53ee4a1107c70d7fff73cad7bf0d0.js"
+  "/ja/precache-manifest.42a08856ee9b903ff981548170182964.js"
 );
 
 workbox.clientsClaim();
